c1a0cfe964b372f55b76d06cde19ecc64be4ac0a
Jason Liu <jasonliu1999@gmail.com>
2017-09-30 17:56:19 -0400

    Updated c file with test game loop

--- app/src/main/cpp/native-lib.c
+++ app/src/main/cpp/native-lib.c
@@ -1,4 +1,18 @@
 #include <jni.h>
+#include <time.h>
+
+/* Dimensions of screen in pixels is 480 * 800*/
+static double birdPosition = 240;
+static double birdVelocity = -9.8;
+static int loopCondition = 1;
+
+JNIEXPORT jdouble JNICALL
+Java_com_android_andi_androidtest_MainActivity_getBirdPosition(
+        JNIEnv *env,
+        jobject obj) {
+    return birdPosition;
+}
+
 
 JNIEXPORT jstring JNICALL
 Java_com_android_andi_androidtest_MainActivity_stringFromJNI(
@@ -6,4 +20,29 @@ Java_com_android_andi_androidtest_MainActivity_stringFromJNI(
         jobject obj) {
     char hello[] = "Hello from C";
     return (*env)->NewStringUTF(env, hello);
+}
+
+//Main loop
+JNIEXPORT void JNICALL
+Java_com_android_andi_androidtest_MainActivity_startGame(
+        JNIEnv *env,
+        jobject obj) {
+
+    clock_t begin = clock();
+    struct timespec sleepTime, interruptTime;
+    sleepTime.tv_sec = 0;
+    sleepTime.tv_nsec = 417000000;
+
+    while(loopCondition){
+        if (birdVelocity < -50)
+            birdVelocity = 0;
+        if (birdPosition < 0)
+            birdPosition = 300;
+        birdPosition +=birdVelocity/24;
+        birdVelocity -= 0.5;
+        nanosleep(&sleepTime, &interruptTime);
+    }
+
+
+
 }
